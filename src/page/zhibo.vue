548c392f2401f631a16459c9c23ea0ef443c96ff
0730chen <1252331215@qq.com>
2019-07-31 18:45:49 +0800

    评论界面

--- src/page/zhibo.vue
+++ src/page/zhibo.vue
@@ -5,13 +5,23 @@
         <div class="comments" v-for="(comment,index) in commentsList" :key="index">
             <img :src="comment.sharePicUrl">
             <div class="title">{{comment.title}}</div>
+            <div class="context" v-for="(context,index) in comment.text" :key="index" v-if ="comment.text !=[]">
+                {{context}}
         </div>
         </div>
     
     <footBar></footBar>
     </div>
+    </div>
 </template>
 <style scoped>
+.context{
+    position:absolute;
+    width:100%;
+    height:0.3rem;
+    font-size:0.16rem;
+    transform:translateY(0.5rem)
+}
 .title{
     position: absolute;
     left: 0.5rem;
@@ -31,7 +41,7 @@ img{
     height: 2rem;
 }
 .containe{
-        
+        position:relative;
         font-size: 0.16rem;
         justify-content: start;
     }
@@ -55,9 +65,15 @@ export default {
             axios.get('http://127.0.0.1:3000/hot/topic?limit=30&offset=30',{withCredentials:true}).then(res => {
                 console.log(res.data.hot)
                 let list = res.data.hot
-                this.commentsList = res.data.hot
+                // this.commentsList = res.data.hot
                 for(const iter of list){
-                    console.log(iter)
+                    if(iter.text.length != 0){
+                        this.commentsList.push(iter)
+                        console.log(iter.text)
+                    }else{
+                        console.log('没有留言')
+                    }
+                    // console.log(typeof(iter.text))
                 }
             })
         }
