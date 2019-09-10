ed9122bf1dbc22c945c95f745278c8a450337d91
0730chen <1252331215@qq.com>
2019-09-10 20:00:09 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -16,11 +16,11 @@ const store = new Vuex.Store({
         count: 3,
         list: [{ 'link': '1234' }, { 'ssss': 'zzz' }]
     },
-    //同步代码定义，使用commit('方法'，参数)调用并传参
+    
     mutations: {
         increment(state) {
             state.count++
-                // console.log(state.count)
+              
         },
         print(state, name) {
             state.count = 10
@@ -36,20 +36,12 @@ const store = new Vuex.Store({
             store.commit('fitter', a.data.result)
             return a
         }
-        // incrementAsync({ commit }) {
-        //     setTimeout(() => {
-        //         store.commit('increment')
-        //         console.log(store.state.count)
-        //             // console.log(store.count)
-        //     }, 5000);
-        // }
+        
     }
 })
 store.commit('print', 'jack')
 console.log(store.state.count)
-    // store.commit('increment')
-    // console.log(store.state.list[0].link)
-    /* eslint-disable no-new */
+    
 new Vue({
     el: '#app',
     router,
