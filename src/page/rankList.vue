42ff779402467d1005e39e99b4bab76cdc835714
0730chen <1252331215@qq.com>
2020-02-21 13:46:01 +0800

    修改ListRank组件

--- src/page/rankList.vue
+++ src/page/rankList.vue
@@ -1,29 +1,49 @@
 <template>
-    <div>
-        <div class="rankList">
-            <backTab></backTab>
-            <div v-for="(song,index) in rankList" :key="index">
-                <div class="songList">
-                    <img class="rankImg" v-bind:src="song.coverImgUrl" alt="排行"><span class="updata">{{song.updateFrequency}}</span><span :data-id = "song.id" @click="onchange">{{song.name}}</span>
-                </div>
-                <div class="block"></div>
-            </div>
+  <div class="RankList-wrapper">
+    <footBar></footBar>
+    <div class="rankList">
+      <div v-for="(song,index) in rankList" :key="index" class="songList-wrapper">
+        <div class="songList">
+          <img class="rankImg" v-bind:src="song.coverImgUrl" alt="排行"><span
+          class="updata">{{song.updateFrequency}}</span><span :data-id="song.id" @click="onchange">{{song.name}}</span>
         </div>
-        <footBar></footBar>
+      </div>
     </div>
+    <div class="RankList-title">
+      <backTab></backTab>
+      <span class="title">排行榜</span>
+    </div>
+  </div>
 </template>
 <style scoped>
-.updata{
+  .RankList-wrapper {
+    display: flex;
+    flex-direction: column-reverse;
+    font-size: 16px;
+  }
+
+  .RankList-title {
+    display: flex;
+    flex-direction: row;
+    margin-top: 8px;
+  }
+
+  .title {
+    margin: 0 auto;
+  }
+
+  .updata {
+    color: aliceblue;
     position: absolute;
-    width: 1rem;
-    height: 0.5rem;
-    z-index: 10;
-    left: 0;
-    color:aliceblue;
-    text-align: center;
-    transform: translateY(0.5rem)
-}
-.songList{
+    left: 10px;
+    top: 75px;
+  }
+
+  .songList-wrapper {
+    padding: 8px 0;
+  }
+
+  .songList {
     height: 1rem;
     width: 3.45rem;
     display: flex;
@@ -31,80 +51,72 @@
     justify-content: start;
     align-items: center;
     margin-left: 10px;
-}
-.rankImg {
+    padding: 8px 0px;
+    position: relative;
+  }
+
+  .rankImg {
     height: 1rem;
     width: 1rem;
-}
-span{
-    position: absolute;
-    widows: 1.7rem;
-    height: 0.5rem;
-    text-align: center;
-    left: 30%;
-    transform: translateY(0.5rem);
-    font-size: 0.16rem
-}
-.block{
+    margin-right: 12px;
+  }
+
+  span {
+  }
+
+  .block {
     display: block;
     width: 100%;
     height: 0.2rem;
     color: aliceblue;
-}
-.rankList{
-    top: 0;
-    transform: translateY(-10px)
-}
-.back{
-    top:-0.31rem !important;
-    left:-0.07rem !important;
-}
+  }
+
+  .rankList {
+    margin-top: 20px;
+    height: 600px;
+    overflow: auto;
+  }
+
+  .back {
+  }
 
 </style>
 
 
 <script>
-import axios from 'axios'
-import footBar from '../components/FooterBuild'
-import backTab from '../components/BackPage'
-export default {
-    // rankList:'',
-    data() {
-        return {
-        name:"ranklist",
-        rankList:[],
-        // that:''
-        }
-    },
-    methods: {
-        getData(){
-            let self = this
-            axios.get('api/toplist').then(function(res){
+    import axios from 'axios'
+    import footBar from '../components/FooterBuild'
+    import backTab from '../components/BackPage'
 
-                // console.log(res.data)
-                self.rankList = res.data['list']
-                // self.rankList = res.data.playlist['tracks']
-                // console.log(rankList)
-                //  this.rankList.append(res.data.playlist['tracks'])
-                //  console.log(this.rankList)
-                // this.rankList = res.data.playlist['tracks']
-                // console.log(this.rankList)
-            }).catch(function(error){
-                console.log(error)
-            })
+    export default {
+        // rankList:'',
+        data() {
+            return {
+                name: "ranklist",
+                rankList: [],
+                // that:''
+            }
         },
-        onchange(e){
-            // console.log(e.target.dataset.id)
-            let id = e.target.getAttribute('data-id')
-            console.log(id)
-            // axios.get('http://127.0.0.1:3000/toplist')
+        methods: {
+            getData() {
+                let self = this
+                axios.get('api/toplist').then(function (res) {
+
+                    self.rankList = res.data['list']
+                }).catch(function (error) {
+                    console.log(error)
+                })
+            },
+            onchange(e) {
+                let id = e.target.getAttribute('data-id')
+                console.log(id)
+            }
+        },
+        created() {
+            this.getData()
+        },
+        components: {
+            footBar, backTab
         }
-    },
-    created() {
-        this.getData()
-    },
-    components:{
-        footBar,backTab
     }
-}
 </script>
