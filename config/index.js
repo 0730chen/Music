e2757cc210083435f32f47af2e25b3712d23d9c2
0730chen <1252331215@qq.com>
2020-08-24 22:06:09 +0800

    feat:修改接口

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://www.chentian.ltd/music/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
